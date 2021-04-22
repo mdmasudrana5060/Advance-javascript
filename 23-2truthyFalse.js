5643ffce715fcdc2df2593c10341ae5bbb4613b4
mdmasudrana5060 <ranam5060@gmail.com>
2021-04-23 01:19:30 +0600

    newthing

---
+++ 23-2truthyFalse.js
@@ -0,0 +1,13 @@
+// some false value in javascript
+// 1.0
+// 2.null
+// 3.undefined
+// 4.NaN
+// 5.false
+function add(num1,num2){
+    const result =num1+num2;
+    return result;
+}
+const value=add(10,12);
+console.log(value);
+console.log("kashem");
